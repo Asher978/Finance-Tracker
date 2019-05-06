b5e8084ea15faa9d0b3545438eb5a574642f1094
Asher Shaheen <ashershaheen@gmail.com>
2019-05-06 00:09:47 -0400

    added a serach method which takes params from the form

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,17 @@
+class StocksController < ApplicationController
+
+    def search
+        if params[:stock].present?
+            @stock = Stock.new_from_lookup(params[:stock])
+            if @stock
+                render "users/my_portfolio"
+            else
+                flash[:danger] = "You have entered an incorrect symbol!"
+                redirect_to my_portfolio_path    
+            end
+        else
+            flash[:danger] = "You must provide a stock symbol!"
+            redirect_to my_portfolio_path
+        end
+    end
+end
