7ccb10789ef23cf949fe5e5e946366ccacf5a76a
Asher Shaheen <ashershaheen@gmail.com>
2019-05-06 00:57:41 -0400

    refactored the if else statements

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,25 +1,15 @@
 class StocksController < ApplicationController
 
     def search
-        if params[:stock].present?
-            @stock = Stock.new_from_lookup(params[:stock])
-            if @stock
-                respond_to do |format|
-                    format.js { render partial: "users/result" }
-                end
-            else
-                respond_to do |format|
-                    flash.now[:danger] = "You have entered an incorrect symbol!"
-                    format.js { render partial: "users/result" }
-                end
-                
-            end
+        if params[:stock].blank?
+            flash.now[:danger] = "You must provide a stock symbol!"
         else
-            respond_to do |format|
-                flash.now[:danger] = "You must provide a stock symbol!"
-                format.js { render partial: "users/result" }
-            end
+            @stock = Stock.new_from_lookup(params[:stock])
+            flash.now[:danger] = "You have entered an incorrect symbol!" unless @stock
+        end
+        respond_to do |format|
+            format.js { render partial: "users/result" }
         end
     end
-    
+
 end
