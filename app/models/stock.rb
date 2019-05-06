b3ab7a40fb4097e99a9010af2a66fe6ff284b439
Asher Shaheen <ashershaheen@gmail.com>
2019-05-06 00:12:34 -0400

    stock model method which looks up the stock and return a new object with received values

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+    def self.new_from_lookup(ticker_symbol)
+        begin
+            looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+            new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.previous_close)
+        rescue Exception => e
+            return nil
+        end
+    end
 end
