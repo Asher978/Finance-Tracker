a70c4f2cdc9c51694522182478d8a70bd3145b68
Asher Shaheen <ashershaheen@gmail.com>
2019-05-06 20:34:25 -0400

    Api Helper

---
+++ client/src/helpers/ApiClient.js
@@ -0,0 +1,48 @@
+import fetch from "isomorphic-fetch";
+
+// TODO Move me elsewhere!
+const host = window.location.protocol + "//" + window.location.host;
+
+export default class clientAPI {
+  constructor() {
+    this.headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+  }
+
+  get(url, params) {
+    const fullUrl = new URL(host + url);
+
+    // Optional query parameters.
+    if (params) {
+      Object.keys(params).forEach(key =>
+        fullUrl.searchParams.append(key, params[key])
+      );
+    }
+
+    return fetch(fullUrl, {
+      method: "GET",
+      headers: this.headers,
+      credentials: "include"
+    });
+  }
+
+  post(url, params) {
+    return fetch(host + url, {
+      method: "POST",
+      headers: this.headers,
+      credentials: "include",
+      body: JSON.stringify(params.data)
+    });
+  }
+
+  put(url, params) {
+    return fetch(host + url, {
+      method: "PUT",
+      headers: this.headers,
+      credentials: "include",
+      body: JSON.stringify(params.data)
+    });
+  }
+}
