e5fc77ddc88a77d475492687c523e7f6d4633534
Asher Shaheen <ashershaheen@gmail.com>
2019-05-09 05:31:42 -0400

    clean up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,11 @@
 module Api
-    module V1
-        class UsersController < ApplicationController
-        
-            def my_portfolio
-        
-            end 
-        
-            def index
-                @users = User.all
-        
-                render json: @users
-                
-            end
-        
-        end
+  module V1
+    class UsersController < ApplicationController
+      def my_portfolio
+      end
+
+      def index
+      end
     end
+  end
 end
