13f8c50beb53a9167c6dd6c37357a2b864d2c41e
Asher Shaheen <ashershaheen@gmail.com>
2019-05-09 05:30:24 -0400

    send response as Rails api instead of responding formulated for rails views

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class StocksController < ApplicationController
       before_action :authenticate_user!
+
       def create
         @stocks = Stock.all
         render json: @stocks
@@ -16,14 +17,13 @@ module Api
 
       def search
         if params[:stock].blank?
-          flash.now[:danger] = 'You must provide a stock symbol!'
+          return render json: { error: "You must provide a stock symbol!" }
         else
           @stock = Stock.new_from_lookup(params[:stock])
-          flash.now[:danger] = 'You have entered an incorrect symbol!' unless @stock
+          return render json: { error: "You have entered an incorrect symbol!" } unless @stock
         end
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
+
+        render json: @stock
       end
     end
   end
