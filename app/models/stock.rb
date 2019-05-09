53cfb26970dfa18e357ab8f90adc26b08adc3fe0
Asher Shaheen <ashershaheen@gmail.com>
2019-05-09 05:30:48 -0400

    comments

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,10 +4,16 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  # look up a stock based on user input
+  # and create a new object. Since the response
+  # is pretty large from this look up, hence we are
+  # only saving the necessary fields
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.previous_close)
-  rescue Exception => e
-    nil
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.previous_close)
+    rescue Exception => e
+      return nil
+    end
   end
 end
